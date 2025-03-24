60b3bcb3c230cadd367b48e3966c955f9408e29b
devYavya <yavya.sharma21@gmail.com>
2025-03-24 23:17:04 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Screens/Home";
 import ServicesPage from "./components/Services/Services";
 import Navbar from "./components/ui/NavBar";
@@ -11,19 +11,30 @@ import UnderConstruction from "./components/ui/UnderContruction";
 import OurWork from "./Screens/OurWork";
 import PrivacyPolicy from "./components/Privacy/PrivacyPolicies";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+};
+
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<ServicesPage />} />
-        <Route path="/contact" element={<ContactPage />}></Route>
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/solutions" element={<ExpertiseSection />} />
         <Route path="/testimonials" element={<Testimonials />} />
         <Route path="/our-work" element={<OurWork />} />
         <Route path="/Privacy-Policy" element={<PrivacyPolicy />} />
-        <Route path="/ServiceTerms" element={<UnderConstruction/>} />
+        <Route path="/ServiceTerms" element={<UnderConstruction />} />
       </Routes>
       <Footer />
     </>
